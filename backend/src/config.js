37f5c37c2b65946a148acff8cf817c80c774dcdd
Ravenbishop87 <redeemerbux@gmail.com>
2022-03-29 08:38:18 -0400

    commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -76,7 +76,7 @@ let CONTRACT_ADDRESS = "0x4143705b6E27892b4c29cd992690b2DFea8E8e57"; // If you w
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
 const GENERIC_DESCRIPTION = "What Whale Will You Get"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bc9fac91538f57ea20b0664f73b9b7c85b11e95b"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
