0dc9138cb0ec2814405609f3ccc2e32230e8c379
Ravenbishop87 <redeemerbux@gmail.com>
2022-03-29 09:12:25 -0400

    commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -73,7 +73,7 @@ const PRESALE_WHITELISTED_ADDRESSES = ["0x318dB5c91361d1b47115A58E1Fa1955b0041f2
 let CONTRACT_ADDRESS = "0x4143705b6E27892b4c29cd992690b2DFea8E8e57"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
-const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
+const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
 const GENERIC_DESCRIPTION = "What Whale Will You Get"; // Replace with what you want the generic descriptions to say.
 const GENERIC_IMAGE = "https://ipfs.io/ipfs/bc9fac91538f57ea20b0664f73b9b7c85b11e95b"; // Replace with your generic image that will display for all NFTs pre-reveal.
